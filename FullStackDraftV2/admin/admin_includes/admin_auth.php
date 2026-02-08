5a04238763d77051b7c8cc349cf6f0a8b08c01bc
Nico <riveraent05@gmail.com>
2026-02-08 09:46:25 +0800

    Delete admin_auth.php

--- FullStackDraftV2/admin/admin_includes/admin_auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-// admin access guard
-session_start();
-
-// if not logged in as admin, block access
-if (!isset($_SESSION['admin_logged_in'])) {
-    header("Location: admin_login.php");
-    exit;
-}
