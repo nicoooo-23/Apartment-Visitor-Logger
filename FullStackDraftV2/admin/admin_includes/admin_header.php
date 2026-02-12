11a0f752f2b572ebc463d7925363b3db78e78da0
Nico <riveraent05@gmail.com>
2026-02-12 22:01:06 +0800

    added navbar icons

--- FullStackDraftV2/admin/admin_includes/admin_header.php
+++ FullStackDraftV2/admin/admin_includes/admin_header.php
@@ -1,13 +1,12 @@
 <head>
     <link rel="stylesheet" href="../assets/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
     <nav class="nav-bar">
-        <a href="../index.php" class="change-user-btn">Home</a>
-        <a href="../admin/admin_dashboard.php" class="change-user-btn">Dashboard</a>
-        <a href="../admin/apartment.php" class="change-user-btn">Apartments</a>
-        <a href="../admin/visitor_history.php" class="change-user-btn">Visitor History</a>
-        <a href="../admin/logout.php" class="change-user-btn" style="color:red;">
-            Logout
-        </a>
+        <a href="../index.php"><i class="fa-solid fa-house icon"></i>Home</a>
+        <a href="../admin/admin_dashboard.php"><i class="fa-solid fa-tachometer-alt icon"></i>Dashboard</a>
+        <a href="../admin/apartment.php"><i class="fa-solid fa-building icon"></i>Apartments</a>
+        <a href="../admin/visitor_history.php"><i class="fa-solid fa-history icon"></i>Visitor History</a>
+        <a href="../admin/logout.php" style="color:red;"><i class="fa-solid fa-sign-out-alt icon"></i>Logout</a>
     </nav>
 
