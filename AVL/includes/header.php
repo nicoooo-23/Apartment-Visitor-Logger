9bfe1f661a7fe7eb3c1ced6b7bb1981351cc95c4
Graciee <graciela.gozum4@gmail.com>
2026-02-18 19:04:00 +0800

    Added Logo and Tagline

--- AVL/includes/header.php
+++ AVL/includes/header.php
@@ -13,12 +13,21 @@ if (session_status() === PHP_SESSION_NONE) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
 <body>
-
     <hr>
     <nav class="nav-bar">
-        <a href="index.php"><i class="fa-solid fa-house icon"></i> Home</a> |
-        <a href="visitor.php"><i class="fa-solid fa-bell icon"></i> Log Visitors</a> |
-        <a href="admin/admin_dashboard.php"><i class="fa-solid fa-user icon"></i> Admin</a>
+        <div class="title-logo">
+            <div class="logo-container">
+                <img src="assets/AVLLogo.png" alt="AVL Logo" class="logo-image">
+            <div class="title">AVL</div>
+            <div class="tagline">
+                Secure. Record. Manage Every Visitor.
+            </div>
+        </div>
+        <div class="nav-links">
+            <a href="index.php"><i class="fa-solid fa-house icon"></i> Home</a>
+            <a href="visitor.php"><i class="fa-solid fa-bell icon"></i> Log Visitors</a>
+            <a href="admin/admin_dashboard.php"><i class="fa-solid fa-user icon"></i> Admin</a>
+        </div>
     </nav>
     <hr>
 
