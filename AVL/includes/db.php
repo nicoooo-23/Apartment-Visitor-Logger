db6cca860020b21906812f14887ea2f20a3e8e92
Nico <riveraent05@gmail.com>
2026-03-13 16:04:40 +0800

    Update db.php

--- AVL/includes/db.php
+++ AVL/includes/db.php
@@ -19,6 +19,9 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+// Lines starting from this point are to make sure the database exists even without importing an sql dump file
+// Database will be created along with the tables needed (IF NOT EXISTS used to reduce errors if database already exists beforehand
+
 // create database if it doesn't exist
 $conn->query("CREATE DATABASE IF NOT EXISTS $dbname");
 
@@ -89,6 +92,7 @@ $conn->query("
 // AUTO-CREATE DEFAULT ADMIN USER
 // ==============================
 
+// For testing purposes, default admin user is created (In actual deployment/implementation, this will be omitted)
 // default admin credentials
 $default_username = "admin";
 $default_password = "admin123"; // change if you want
